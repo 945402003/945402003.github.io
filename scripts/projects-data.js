8ee9673cc35e469b00101493f6eb84cdb4538041
杨正一 <yzy@MacBook-Air-2.local>
2026-03-04 18:08:16 +0800

    add projects issue

--- scripts/projects-data.js
+++ scripts/projects-data.js
@@ -2,12 +2,21 @@
   // similar structure to publicationData but for projects
   window.projectData = [
     {
-      image: { src: "", alt: "Example project teaser" },
+      image: { src: "images/portait.jpg", alt: "Example project teaser" },
       title: "Sample Project Title",
       authors: "<strong>Zhengyi Yang</strong>",
       venue: "Demo description or role",
       links: [
-        { href: "https://example.com/project", text: "project page", className: "project", icon: "fa fa-link" }
+        { href: "#", text: "project page", className: "project", icon: "fa fa-link" }
+      ]
+    },
+    {
+      image: { src: "images/Tencent_logo.png", alt: "Tencent logo" },
+      title: "Tencent Hunyuan",
+      authors: "<strong>Zhengyi Yang</strong>",
+      venue: "Multimodal Reinforcement Learning",
+      links: [
+        { href: "#", text: "learn more", className: "project", icon: "fa fa-link" }
       ]
     }
   ];
