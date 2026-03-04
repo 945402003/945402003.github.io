2be204985fc4e5287c8aaeae46192bae2a756c0e
杨正一 <yzy@MacBook-Air-2.local>
2026-03-04 10:47:04 +0800

    for project

--- scripts/awards-data.js
+++ scripts/awards-data.js
@@ -1,9 +1,5 @@
 (() => {
   window.awardsData = [
     '<strong>"Innovation Breakthrough" Zhenyi Scholarship</strong>, 2025 (PhD student, Shanghai Innovation Institute)',
-    '<strong>National Scholarship</strong>, 2025 (PhD student)',
-    '<strong>Future Star Honor</strong>, 2024 (Research intern, Shanghai Academy of AI for Science)',
-    '<strong>National Scholarship</strong>, 2024 (PhD student)',
-    '<strong>National Scholarship</strong>, 2023 (undergraduate student)'
   ];
 })();
