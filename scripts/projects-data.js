9e56a5b16b9a67dce95960234c1a55eb02e54694
杨正一 <yzy@MacBook-Air-2.local>
2026-03-04 17:53:07 +0800

    add intro

--- scripts/projects-data.js
+++ scripts/projects-data.js
@@ -2,7 +2,7 @@
   // similar structure to publicationData but for projects
   window.projectData = [
     {
-      image: { src: "images/example-project.png", alt: "Example project teaser" },
+      image: { src: "", alt: "Example project teaser" },
       title: "Sample Project Title",
       authors: "<strong>Zhengyi Yang</strong>",
       venue: "Demo description or role",
